ac286ea0e22b1badfe9c8c4914294de364862153
Khyle Matthew Gopole <91583649+khygopole@users.noreply.github.com>
2021-09-29 12:04:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//Hello GALAXY
 int main()
 {
     int firstNumber, secondNumber, sumOfTwoNumbers, diffOfTwoNumbers, prodOfTwoNumbers, quotOfTwoNumbers, modOfTwoNumbers;
